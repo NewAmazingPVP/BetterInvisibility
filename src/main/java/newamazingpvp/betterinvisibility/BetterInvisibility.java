e72a782225500b987d3c128104823e2c4bb37fdc
NewAmazingPVP <gamingofpat@gmail.com>
2023-05-16 19:43:13 -0400

    1.13+ compatible

--- src/main/java/newamazingpvp/betterinvisibility/BetterInvisibility.java
+++ src/main/java/newamazingpvp/betterinvisibility/BetterInvisibility.java
@@ -27,7 +27,9 @@ public final class BetterInvisibility extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        // Initialize ProtocolManager
         protocolManager = ProtocolLibrary.getProtocolManager();
+        // Register events for this plugin
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -36,26 +38,29 @@ public final class BetterInvisibility extends JavaPlugin implements Listener {
         Entity entity = event.getEntity();
         if (entity instanceof Player) {
             Player player = (Player) entity;
-            PotionEffectType newEffectType = event.getNewEffect().getType();
-            PotionEffectType oldEffectType = event.getOldEffect().getType();
-
-            if (newEffectType.equals(PotionEffectType.INVISIBILITY)) {
+            if (event.getNewEffect() != null && event.getNewEffect().getType() != null && event.getNewEffect().getType().equals(PotionEffectType.INVISIBILITY)) {
+                // Schedule a task to remove all armor when player becomes invisible
                 Bukkit.getScheduler().runTaskTimer(this, () -> removeAllArmor(player), 0L, 0L);
-            } else if (oldEffectType.equals(PotionEffectType.INVISIBILITY)) {
+            } else if (event.getOldEffect() != null && event.getOldEffect().getType() != null && event.getOldEffect().getType().equals(PotionEffectType.INVISIBILITY)) {
+                // Schedule a task to restore player's armor when invisibility effect is removed
                 Bukkit.getScheduler().runTaskTimer(this, () -> restoreArmor(player), 0L, 0L);
             }
         }
     }
 
+
     public void restoreArmor(Player player) {
+        // Create a packet to restore player's armor
         PacketContainer restoreArmorPacket = protocolManager.createPacket(PacketType.Play.Server.ENTITY_EQUIPMENT);
         restoreArmorPacket.getIntegers().write(0, player.getEntityId());
 
+        // Get the player's armor contents and held items
         ItemStack[] armorContents = player.getInventory().getArmorContents();
         ItemStack mainHand = player.getInventory().getItemInMainHand();
         ItemStack offHand = player.getInventory().getItemInOffHand();
 
-        EnumWrappers.ItemSlot[] slots = new EnumWrappers.ItemSlot[] {
+        // Define the slots where the armor and held items are equipped
+        EnumWrappers.ItemSlot[] slots = new EnumWrappers.ItemSlot[]{
                 EnumWrappers.ItemSlot.FEET,
                 EnumWrappers.ItemSlot.LEGS,
                 EnumWrappers.ItemSlot.CHEST,
@@ -64,10 +69,11 @@ public final class BetterInvisibility extends JavaPlugin implements Listener {
                 EnumWrappers.ItemSlot.OFFHAND
         };
 
+        // Create a list of slot-item pairs for the packet
         List<Pair<EnumWrappers.ItemSlot, ItemStack>> slotItemPairs = new ArrayList<>();
         for (int i = 0; i < slots.length; i++) {
             EnumWrappers.ItemSlot itemSlot = slots[i];
-            ItemStack item = null;
+            ItemStack item;
             if (itemSlot == EnumWrappers.ItemSlot.MAINHAND) {
                 item = mainHand;
             } else if (itemSlot == EnumWrappers.ItemSlot.OFFHAND) {
@@ -79,7 +85,7 @@ public final class BetterInvisibility extends JavaPlugin implements Listener {
             slotItemPairs.add(slotItemPair);
         }
         restoreArmorPacket.getSlotStackPairLists().write(0, slotItemPairs);
-
+        // Send the restore armor packet to all players in the same world
         List<Player> playersInWorld = player.getWorld().getPlayers();
         for (Player currentPlayer : playersInWorld) {
             try {
@@ -94,10 +100,21 @@ public final class BetterInvisibility extends JavaPlugin implements Listener {
 
 
     public void removeAllArmor(Player player) {
+        // Create a packet to clear player's armor
         PacketContainer clearArmorPacket = protocolManager.createPacket(PacketType.Play.Server.ENTITY_EQUIPMENT);
         clearArmorPacket.getIntegers().write(0, player.getEntityId());
 
-        List<EnumWrappers.ItemSlot> slots = new ArrayList<>(Arrays.asList(EnumWrappers.ItemSlot.FEET, EnumWrappers.ItemSlot.LEGS, EnumWrappers.ItemSlot.CHEST, EnumWrappers.ItemSlot.HEAD, EnumWrappers.ItemSlot.MAINHAND, EnumWrappers.ItemSlot.OFFHAND));
+        // Define the slots where the armor and held items are equipped
+        List<EnumWrappers.ItemSlot> slots = new ArrayList<>(Arrays.asList(
+                EnumWrappers.ItemSlot.FEET,
+                EnumWrappers.ItemSlot.LEGS,
+                EnumWrappers.ItemSlot.CHEST,
+                EnumWrappers.ItemSlot.HEAD,
+                EnumWrappers.ItemSlot.MAINHAND,
+                EnumWrappers.ItemSlot.OFFHAND
+        ));
+
+        // Create a list of slot-item pairs with empty items (air) for the packet
         List<Pair<EnumWrappers.ItemSlot, ItemStack>> slotItemPairs = new ArrayList<>();
         for (int i = 0; i < slots.size(); i++) {
             EnumWrappers.ItemSlot itemSlot = slots.get(i);
@@ -107,8 +124,8 @@ public final class BetterInvisibility extends JavaPlugin implements Listener {
         }
         clearArmorPacket.getSlotStackPairLists().write(0, slotItemPairs);
 
+        // Send the clear armor packet to all players in the same world
         List<Player> playersInWorld = player.getWorld().getPlayers();
-
         for (Player currentPlayer : playersInWorld) {
             try {
                 if (currentPlayer != player) {
